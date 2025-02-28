4848b60ec33fee180ac8abc5f1dd59476e70b8b8
AgnesOlsen <s224381@dtu.dk>
2025-02-28 15:18:46 +0100

    Histograms and imputation of missing values

--- Access_data.py
+++ Access_data.py
@@ -1,40 +1,50 @@
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 # Firstly we read the data and create a dataframe using pandas
 
 data = pd.read_csv('processed_cleveland.csv')
 
 print(data.head())
 
+print(data.describe())
+
 df = pd.DataFrame(data)
-#for checking the amount of ? in the data
-print("amount ? in the data =", (df == "?").sum().sum()) 
+
+#Checking for missing value
+print("Amount of missing values in the different attributes", (df == "?").sum()) 
+
+#Handling missing values by applying mode impuation 
+df["ca"] = df["ca"].replace("?", df["ca"].mode()[0])
+df["thal"] = df["thal"].replace("?", df["thal"].mode()[0])
+print("most common value in ca = ",df["ca"].mode()[0])
+print("most common value in thal =",df["thal"].mode()[0])
+#Checking that there is no longer any missing values in the attribues
+print("Amount of missing values in the different attributes after imputation", (df == "?").sum())
+
+#Checking the data types of the different attributes and insuring they are all float64
+print("datatype for collumn that hasent been imputed: oldpeak type:", df["oldpeak"].dtype)
+#checing the imputed columns data type
+print("ca type:", df["ca"].dtype)
+print("thal type:", df["thal"].dtype)
+
+#Changing the imputed attributes types to float64
+df["ca"] = pd.to_numeric(df["ca"], errors="coerce")  
+df["thal"] = pd.to_numeric(df["thal"], errors="coerce")  
+print("ca new type", df["ca"].dtype)  
+print("thal new type", df["thal"].dtype)
 
 
 
-#print("amount of ? count=", df.count("?"))
-## We iterate through the data and change any ineligible datapoints with 0.0, this value might need to be changed later
-for row in df.iterrows():
-    for val in row:
-            df.replace(('?',''), 0.0, inplace=True)
-
-print(df)
-
+#Histogram of the different attributes
+plt.figure(figsize=(20,20), dpi=300)
+plt.suptitle('Histogram of the different attributes', fontsize=20)
+plt.tight_layout()
+df.hist()
+plt.savefig('histogram_attributes.png')
 
 # We save the cleaned data to a new csv file
-
 df.to_csv('cleaned_cleveland.csv')
 
-# We do a small safety check to ensure no off values
-
-safetyCheck = open('cleaned_cleveland.csv')
-
-weirdCount = 0
-
-for line in safetyCheck:
-    for val in line.split(','):
-        if val == '?':
-            weirdCount += 1
 
 
-print("amount of ?=", weirdCount)
