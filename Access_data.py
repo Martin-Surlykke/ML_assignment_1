05ef118a377a5ee8f63dd327e1f943c54209151e
Martin Holme Surlykke <s224793@dtu.dk>
2025-02-28 16:46:14 +0100

    Good changes

--- Access_data.py
+++ Access_data.py
@@ -1,10 +1,10 @@
+import numpy as np
 import pandas as pd
 
 # Firstly we read the data and create a dataframe using pandas
 
 data = pd.read_csv('processed_cleveland.csv')
 
-print(data.head())
 
 df = pd.DataFrame(data)
 
@@ -14,7 +14,6 @@ for row in df.iterrows():
     for val in row:
         df.replace(('?',''), 0.0, inplace=True)
 
-print(df)
 
 # We save the cleaned data to a new csv file
 
@@ -22,13 +21,5 @@ df.to_csv('cleaned_cleveland.csv')
 
 # We do a small safety check to ensure no off values
 
-safetyCheck = open('cleaned_cleveland.csv')
+safetyCheck = pd.read_csv('cleaned_cleveland.csv')
 
-weirdCount = 0
-
-for line in safetyCheck:
-    for val in line.split(','):
-        if val == '?':
-            weirdCount += 1
-
-print(weirdCount)
--- Correlation_matrix.py
+++ Correlation_matrix.py
@@ -1,18 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.plotting import scatter_matrix
 import seaborn as sns
-import scipy as sc
 
 def normalize_data(df):
     # We extract a list of mean values for each column
-    meanList = df.mean()
+    mean_list = df.mean()
     # We create an empty dataframe
     df_0 = pd.DataFrame()
 
     #For each column, we subtract the corresponding mean
     for i in df.columns:
-        df_0[i] = df[i] - meanList[i]
+        df_0[i] = df[i] - mean_list[i]
 
     #We create a copy of the meanLess dataframe
     df_normalized = pd.DataFrame(df_0)
@@ -27,26 +25,90 @@ def normalize_data(df):
 
 
 def extract_relevant_vals(df):
-    for 
+    df_relevant = pd.DataFrame()
+    for i in df.columns:
+        if df[i].cov(df['num']) < -0.3 or df[i].cov(df['num']) > 0.3:
+            df_relevant[i] = df[i]
+    return df_relevant
+
+def get_continuous_vals(df):
+    df_continuous = pd.DataFrame()
+
+    df_continuous.insert(0, 'age', df['age'])
+    df_continuous.insert(1, 'trestbps', df['trestbps'])
+    df_continuous.insert(2, 'chol', df['chol'])
+    df_continuous.insert(3, 'thalach', df['thalach'])
+    df_continuous.insert(4, 'oldpeak', df['oldpeak'])
+
+    return df_continuous
+
+def get_continuous_vals_with_num(df):
+    df_continuous_with_num = get_continuous_vals(df)
+    df_continuous_with_num.insert(5, 'num', df['num'])
+
+    return df_continuous_with_num
 
 
-def plot_heatmap(df):
-    plt.figure(figsize=(10,10),dpi=300)
+def heatmap(df):
+    resolution = 20*len(df.columns)
+    print(resolution)
+    g = plt.figure(figsize=(10,10),dpi=resolution)
     sns.heatmap(df.corr(), cmap = 'seismic', vmax = 1, vmin = -1,
-            square = True, linewidths=1, annot=True, annot_kws={"size": 10})
+            square = True, linewidths=1, annot=True, annot_kws={"size": 100/len(df.columns)})
     plt.tight_layout()
-    plt.savefig('correlation_heatmap.png')
+    return g
+
+def full_heatmap(df):
+    fig = heatmap(df)
+    fig.savefig('images/Full_heatmap.png')
+    plt.close(fig)
+
+def highest_var_heatmap(df):
+    usable_data = extract_relevant_vals(df)
+    fig = heatmap(usable_data)
+    fig.savefig('images/High_var_heatmap.png')
+    plt.close(fig)
+
+def continuous_heatmap(df):
+    df = get_continuous_vals_with_num(df)
+    fig = heatmap(df)
+    fig.savefig('images/Continuous_heatmap.png')
+    plt.close(fig)
+
+
+def scatter_matrix(df):
+    sns.set_theme(style="white")
+    sns.pairplot(df, diag_kind='kde')
+    plt.savefig('images/Scatter_matrix.png')
+
+def scatter_with_hue(df):
+    useful_cols = get_continuous_vals_with_num(df)
+    sns.set_theme(style="whitegrid")
+    sns.pairplot(useful_cols, diag_kind='kde', palette=sns.color_palette("hls", 5), hue='num')
+
+    plt.savefig('images/scatter_with_hue.png')
+
+def continuous_scatter(df):
+    df = get_continuous_vals(df)
+    sns.set_theme(style="white")
+    sns.pairplot(df, diag_kind='kde', kind = 'reg', plot_kws={'line_kws':{'color':'red'}})
+    plt.savefig('images/Continuous_scatter_matrix.png')
+
+
+def continuous_scatter_with_hue(df):
+    df = get_continuous_vals_with_num(df)
+    useful_cols = get_continuous_vals_with_num(df)
+    sns.set_theme(style="whitegrid")
+    sns.pairplot(useful_cols, diag_kind='kde', palette='viridis', hue='num')
+
+    plt.savefig('images/continuous_scatter_with_hue.png')
 
 
 
-def plot_scatter_matrix(df):
-    plt.figure(figsize = (20,20), dpi = 300)
-    scatter_matrix(df,alpha=0.2, figsize=(15,15), diagonal='kde')
-    plt.tight_layout()
-    plt.savefig('scatter_matrix.png')
+
 
 data = pd.read_csv('cleaned_cleveland.csv',index_col=[0])
+data = normalize_data(data)
 # Firstly, we load in the data into a dataframe.
-df_normal = normalize_data(data)
 
-plot_scatter_matrix(df_normal)
+continuous_scatter_with_hue(data)
